88cb094878a7c4cfc0d131b53239302d7da7b2d3
SethW89 <31844518+SethW89@users.noreply.github.com>
2020-11-01 12:37:54 -0800

    added base functions

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+// Addition function
+function add(a, b) {
+    return a + b;
+}
+
+// Subtraction function
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiplication function
+function multiply(a, b) {
+    return a * b;
+}
+
+// Division function
+function divide(a, b) {
+    if (b === 0) {
+        return 'DIV 0 ERROR';
+    }
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    } else {
+        return 'OP ERROR';
+    }
+
+}
