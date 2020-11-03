454b6d79403784ad12bc14cd3d871f63492609bd
SethW89 <31844518+SethW89@users.noreply.github.com>
2020-11-02 19:01:27 -0800

    Fixed bugs with equals and display updates

--- script.js
+++ script.js
@@ -13,12 +13,111 @@
 const numberBtn = document.getElementsByClassName('number');
 const display = document.getElementById('display');
 display.textContent = '0';
-let currText = display.textContent;
-let prevOperand = '0';
-let currOperand = '0';
-let prevOperator = '+';
+let prevOperand = '';
+let currOperand = '';
+let prevOperator = '';
 let newOperand = true; // Flag for overwriting screen with new number
 
+
+
+function operate(a, b, operator) {
+    console.log(a, operator, b);
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === 'X') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    } else {
+        return 'OP ERROR';
+    }
+
+}
+
+// Add event for each number button
+for (let i = 0; i < numberBtn.length; i++) {
+    numberBtn[i].addEventListener('click', () => {
+        // Send said text content to display
+        if (newOperand) {
+            displayUpdate(numberBtn[i].textContent);
+            newOperand = false;
+        } else {
+            displayUpdate(currOperand + numberBtn[i].textContent);
+        }
+    });
+}
+// Assign other buttons
+const functionBtn = document.getElementsByClassName('functionBtn');
+for (let i = 0; i < functionBtn.length; i++) {
+    functionBtn[i].addEventListener('click', () => {
+        switch (functionBtn[i].textContent) {
+            case '+':
+            case '-':
+            case 'X':
+            case '/':
+                // We do the previous operation, and reassign
+                // the current and past operands and queue the
+                // next operator
+                // if (prevOperator === '') {
+                //     prevOperator = functionBtn[i].textContent;
+                // }
+                //console.log(prevOperand, currOperand, prevOperator);
+                if (prevOperator === '') {
+                    console.log('Hello there.', functionBtn[i].textContent);
+                    prevOperator = functionBtn[i].textContent;
+                    prevOperand = currOperand;
+                    newOperand = true;
+                } else {
+                    console.log(prevOperand, prevOperator, currOperand);
+                    prevOperand = operate(prevOperand, currOperand, prevOperator);
+                    prevOperator = functionBtn[i].textContent;
+                    newOperand = true;
+                    displayUpdate(prevOperand);
+                }
+
+                // Update Display with result of previous operation
+
+                break;
+            case '.':
+                break;
+            case '=':
+                console.log(prevOperand, prevOperator, currOperand);
+                if (prevOperator != '') {
+                    prevOperand = operate(prevOperand, currOperand, prevOperator);
+                    prevOperator = '';
+                    currOperand = ''
+                    newOperand = true;
+                    displayUpdate(prevOperand);
+                }
+                break;
+            case 'CLEAR':
+                stateCleared()
+                break;
+        }
+    })
+
+}
+function clearDisplay() {
+    display.textContent = '';
+}
+function displayUpdate(value) {
+    // If value is not a number...
+    //prevOperand = display.textContent;
+    display.textContent = value;
+    currOperand = display.textContent;
+    console.log(currOperand);
+}
+
+// Reset to initial conditions
+function stateCleared() {
+    display.textContent = '0';
+    prevOperand = '';
+    prevOperator = '';
+    currOperand = '';
+    newOperand = true;
+}
 
 // Addition function
 function add(a, b) {
@@ -40,93 +139,4 @@ function divide(a, b) {
         return 'DIV 0 ERROR';
     }
     return a / b;
-}
-
-
-function operate(a, b, operator) {
-    if (operator === '+') {
-        return add(a, b);
-    } else if (operator === '-') {
-        return subtract(a, b);
-    } else if (operator === 'X') {
-        return multiply(a, b);
-    } else if (operator === '/') {
-        return divide(a, b);
-    } else {
-        return 'OP ERROR';
-    }
-
-}
-
-// get numbers to show on display when button pressed
-//Add event for each number button
-for (let i = 0; i < numberBtn.length; i++) {
-    numberBtn[i].addEventListener('click', () => {
-        // Send said text content to display
-        if (newOperand) {
-            displayUpdate(numberBtn[i].textContent);
-            newOperand = false;
-        } else {
-            displayUpdate(currOperand + numberBtn[i].textContent);
-        }
-    });
-}
-// Assign other buttons
-const functionBtn = document.getElementsByClassName('functionBtn');
-for (let i = 0; i < functionBtn.length; i++) {
-    // if add is selected
-    functionBtn[i].addEventListener('click', () => {
-        switch (functionBtn[i].textContent) {
-            case '+':
-            case '-':
-            case 'X':
-            case '/':
-                // We do the previous operation, and reassign
-                // the current and past operands and queue the
-                // next operator
-                if (prevOperator === '') {
-                    prevOperator = functionBtn[i].textContent;
-                }
-                console.log(prevOperand, currOperand, prevOperator);
-                prevOperand = operate(prevOperand, currOperand, prevOperator);
-                prevOperator = functionBtn[i].textContent;
-                newOperand = true;
-
-                // Update Display with result of previous operation
-                displayUpdate(prevOperand);
-
-                break;
-            case '.':
-                break;
-            case '=':
-                console.log(prevOperand, currOperand, prevOperator);
-                prevOperand = operate(prevOperand, currOperand, prevOperator);
-                prevOperator = ''
-                newOperand = true;
-                displayUpdate(prevOperand);
-                currOperand = 0;
-                prevOperand = display.textContent;
-                break;
-            case 'CLEAR':
-                stateCleared()
-                break;
-        }
-    })
-
-}
-function clearDisplay() {
-    display.textContent = '';
-}
-function displayUpdate(value) {
-    // If value is not a number...
-    display.textContent = value;
-    currOperand = display.textContent;
-    console.log(currOperand);
-}
-
-function stateCleared() {
-    display.textContent = '0';
-    prevOperand = '0';
-    prevOperator = '+';
-    currOperand = '0';
 }
