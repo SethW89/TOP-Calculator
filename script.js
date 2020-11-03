5c15e40155036c17a43d76b34ad6b4e5b9f520f3
SethW89 <31844518+SethW89@users.noreply.github.com>
2020-11-02 16:15:26 -0800

    div,mul, add, sub buttons work

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 /*
-    If 3 + 2 + 4 = is entered
+    If 3 + 2 - 3 * 5 = | 10 is entered
     3 pressed:    display 3
     + pressed:    display 3
     2 pressed:    display 2
-    + pressed:    display 5
-    4 pressed:    display 4
-    = pressed:    display 9
+    - pressed:    display 5
+    3 pressed:    display 3
+    * pressed:    display 2
+    5 pressed:    display 5
+    = pressed:    display 10;
     clr pressed:  display 0
 */
 const numberBtn = document.getElementsByClassName('number');
 const display = document.getElementById('display');
 display.textContent = '0';
 let currText = display.textContent;
-let operandArr = [];
+let prevOperand = '0';
+let currOperand = '0';
+let prevOperator = '+';
+let newOperand = true; // Flag for overwriting screen with new number
+
 
 // Addition function
 function add(a, b) {
-    return a + b;
+    //console.log('Adding', Number(a) + Number(b));
+    return Number(a) + Number(b);
 }
-
 // Subtraction function
 function subtract(a, b) {
     return a - b;
@@ -42,7 +48,7 @@ function operate(a, b, operator) {
         return add(a, b);
     } else if (operator === '-') {
         return subtract(a, b);
-    } else if (operator === '*') {
+    } else if (operator === 'X') {
         return multiply(a, b);
     } else if (operator === '/') {
         return divide(a, b);
@@ -59,10 +65,15 @@ function operate(a, b, operator) {
 //console.log(numberBtn[0]);
 for (let i = 0; i < numberBtn.length; i++) {
     numberBtn[i].addEventListener('click', () => {
-        console.log(numberBtn[i]);
-        console.log(numberBtn[i].textContent);
+        //console.log(numberBtn[i]);
+        //console.log(numberBtn[i].textContent);
         // Send said text content to display
-        displayUpdate(numberBtn[i].textContent);
+        if (newOperand) {
+            displayUpdate(numberBtn[i].textContent);
+            newOperand = false;
+        } else {
+            displayUpdate(currOperand + numberBtn[i].textContent);
+        }
     });
 }
 // Assign other buttons
@@ -70,17 +81,24 @@ const functionBtn = document.getElementsByClassName('functionBtn');
 for (let i = 0; i < functionBtn.length; i++) {
     //console.log(functionBtn);
     // if add is selected
-    console.log(functionBtn[i].textContent);
+    //console.log(functionBtn[i].textContent);
     functionBtn[i].addEventListener('click', () => {
         switch (functionBtn[i].textContent) {
             case '+':
-
-                break;
             case '-':
-                break;
-            case 'x':
-                break;
+            case 'X':
             case '/':
+                // We do the previous operation, and reassign
+                // the current and past operands and queue the
+                // next operator
+                console.log(prevOperand, currOperand, prevOperator);
+                prevOperand = operate(prevOperand, currOperand, prevOperator);
+                prevOperator = functionBtn[i].textContent;
+                newOperand = true;
+
+                // Update Display with result of previous operation
+                displayUpdate(prevOperand);
+
                 break;
             case '.':
                 break;
@@ -90,30 +108,21 @@ for (let i = 0; i < functionBtn.length; i++) {
                 break;
             case 'CLEAR':
                 display.textContent = '0';
-                operandArr = [];
+                prevOperand = '0';
                 break;
         }
     })
 
-
 }
-
+function clearDisplay() {
+    display.textContent = '';
+}
 function displayUpdate(value) {
     // If value is not a number...
-    if (false) {
-        displayError(); // ToDo
-    }
-    if (value.charCodeAt() >= 48 && value.charCodeAt() <= 57) {
-        //console.log('ascii:', value.charCodeAt());
-        currText = display.textContent;
-        if (currText === '0') {
-            currText = '';
-        }
-        display.textContent = currText + value;
-    }
-    if (value) {
+    //console.log('ascii:', value.charCodeAt());
+    display.textContent = value;
+    currOperand = display.textContent;
 
-    }
     /*
         +      - 43
         =      - 61
